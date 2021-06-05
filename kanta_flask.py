4dd1b06dd520580e7442e484d300e65cd42ce4fb
shaadhussain <hussai19@purdue.edu>
2021-06-04 23:36:36 -0400

    Login/Register still giving CSRF error

--- kanta_flask.py
+++ kanta_flask.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
+# from forms import RegistrationForm, LoginForm
 
+app.config['SECRET KEY'] = 'a13994947cd3942453121e1025360b15'
 posts = [
    
    { 
@@ -27,6 +29,17 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+# @app.route("/register")
+# def register():
+#     form = RegistrationForm()
+#     return render_template('register.html', title="Register", form=form)
+
+# @app.route("/login")
+# def login():
+#     form = LoginForm()
+#     return render_template('login.html', title="Login", form=form)
+
 
 
 if __name__ == '__main__':
