112504716c3111ca8f850130e69e163c2182f7d8
shaadhussain <hussai19@purdue.edu>
2021-07-05 23:10:58 -0400

    Added a tooon. Check the diff

--- kanta_flask/routes.py
+++ kanta_flask/routes.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+from flask import render_template, url_for, flash, redirect
+from kanta_flask import app
+from kanta_flask.models import User, Post   
+from kanta_flask.forms import RegistrationForm, LoginForm
 
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'mysecretkey'
 posts = [
    
    { 
@@ -36,17 +34,18 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success') #"success" here is what's called a category, see layout.html, 28:38 at vid 3
+        flash(f'Account created for {form.username.data}!', 'success') #"success" here is what's called a Bootstrap category, see layout.html, 28:38 at vid 3
         return redirect(url_for('home'))
     return render_template('register.html', title="Register", form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == "password":
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check your credentials.', 'danger')
     return render_template('login.html', title="Login", form=form)
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
